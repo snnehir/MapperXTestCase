8c505be9310f0974fc3955a5227674930c61bc47
snnehir <snnehir21@gmail.com>
2024-03-02 12:23:03 +0300

    project initialization

---
+++ test-app/src/app/app.config.ts
@@ -0,0 +1,14 @@
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
+import { provideRouter } from '@angular/router';
+
+import { routes } from './app.routes';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { environment } from '../environments/environment';
+
+export const appConfig: ApplicationConfig = {
+  providers: [
+    provideRouter(routes), importProvidersFrom(provideFirebaseApp(() => initializeApp(environment.firebase))),
+    importProvidersFrom(provideAuth(() => getAuth()))
+  ]
+};
